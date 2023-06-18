146c4237ccf6e624ca5c26d100d187a48b792d8f
Lejin Tian <ljtian20@fudan.edu.cn>
2023-06-18 08:19:35 +0000

    20230618

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ wget -O /mdshare/node8/txmdata/Phenomics/weekly_online_paper_metrices/SearchResu
 /usr/bin/Rscript /mdshare/node8/txmdata/Phenomics/weekly_online_paper_metrices/5_each_month_IF.R
 
 rm -rf /mdshare/node8/txmdata/Phenomics/Rplots.pdf
+rm -rf /mdshare/node8/txmdata/Phenomics/.Rhistory
 
 git add -A; DATE=$(date "+%Y%m%d"); git commit -m $DATE --no-verify; git push
 
